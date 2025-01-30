13a47db1eedc6b3f3d70ebf25d825d20dfd23185
Paulo Suave <suave.pl@gmail.com>
2025-01-30 14:55:00 -0300

    Creating asset folder Inserting all assets Creating the menu: backgrund, sound and fonts Creating the const file

--- code/Menu.py
+++ code/Menu.py
@@ -5,7 +5,10 @@ import pygame.image
 from pygame import Surface, Rect
 from pygame.font import Font
 
-from code.Const import WIN_WIDTH, C_ORANGE, MENU_OPTION, C_WHITE, C_YELLOW
+from code.Const import WIN_WIDTH, COLOR_ORANGE, COLOR_GREEN, MENU_OPTION, COLOR_YELLOW, COLOR_WHITE
+
+
+# from code.Const import WIN_WIDTH, C_ORANGE, MENU_OPTION, C_WHITE, C_YELLOW
 
 
 class Menu:
@@ -14,42 +17,43 @@ class Menu:
         self.surf = pygame.image.load('./asset/MenuBg.png').convert_alpha()
         self.rect = self.surf.get_rect(left=0, top=0)
 
-    def run(self):
-        menu_option = 0
+    def run(self, ):
+        # menu_option = 0
+        # INSERINDO MUSICA DO MENU
         pygame.mixer_music.load('./asset/Menu.mp3')
         pygame.mixer_music.play(-1)
         while True:
-            # DRAW IMAGES
+            # PARAMETROS DE IMAGEM
             self.window.blit(source=self.surf, dest=self.rect)
-            self.menu_text(50, "Mountain", C_ORANGE, ((WIN_WIDTH / 2), 70))
-            self.menu_text(50, "Shooter", C_ORANGE, ((WIN_WIDTH / 2), 120))
+            self.menu_text(50, "Mountain", COLOR_GREEN, text_center_pos=((WIN_WIDTH / 2), 50))
+            self.menu_text(50, "shooter", COLOR_ORANGE, text_center_pos=((WIN_WIDTH / 2), 80))
+            # pygame.display.flip()
 
             for i in range(len(MENU_OPTION)):
-                if i == menu_option:
-                    self.menu_text(20, MENU_OPTION[i], C_YELLOW, ((WIN_WIDTH / 2), 200 + 25 * i))
-                else:
-                    self.menu_text(20, MENU_OPTION[i], C_WHITE, ((WIN_WIDTH / 2), 200 + 25 * i))
+                self.menu_text(20, MENU_OPTION[i], COLOR_YELLOW, ((WIN_WIDTH / 2), 200 + 25 * i))
+
             pygame.display.flip()
 
-            # Check for all events
+            #         CHECAGEM DE EVENTOS
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()  # Close Window
                     quit()  # end pygame
-                if event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_DOWN:  # DOWN KEY
-                        if menu_option < len(MENU_OPTION) - 1:
-                            menu_option += 1
-                        else:
-                            menu_option = 0
-                    if event.key == pygame.K_UP:  # UP KEY
-                        if menu_option > 0:
-                            menu_option -= 1
-                        else:
-                            menu_option = len(MENU_OPTION) - 1
-                    if event.key == pygame.K_RETURN:  # ENTER
-                        return MENU_OPTION[menu_option]
 
+    #             if event.type == pygame.KEYDOWN:
+    #                 if event.key == pygame.K_DOWN:  # DOWN KEY
+    #                     if menu_option < len(MENU_OPTION) - 1:
+    #                         menu_option += 1
+    #                     else:
+    #                         menu_option = 0
+    #                 if event.key == pygame.K_UP:  # UP KEY
+    #                     if menu_option > 0:
+    #                         menu_option -= 1
+    #                     else:
+    #                         menu_option = len(MENU_OPTION) - 1
+    #                 if event.key == pygame.K_RETURN:  # ENTER
+    #                     return MENU_OPTION[menu_option]
+    # FUNÇÃO DO MENU TEXTO
     def menu_text(self, text_size: int, text: str, text_color: tuple, text_center_pos: tuple):
         text_font: Font = pygame.font.SysFont(name="Lucida Sans Typewriter", size=text_size)
         text_surf: Surface = text_font.render(text, True, text_color).convert_alpha()
--- code/game.py
+++ code/game.py
@@ -1,12 +1,13 @@
 import pygame
 
+from code.Const import WIN_WIDTH, WIN_HEIGHT
 from code.Menu import Menu
 
 
 class Game:
     def __init__(self):
         pygame.init()
-        self.window = pygame.display.set_mode(size=(600, 480))
+        self.window = pygame.display.set_mode(size=(WIN_WIDTH, WIN_HEIGHT))
 
     def run(self):
         while True:
@@ -15,9 +16,6 @@ class Game:
             pass
 
 
-            # Check for all events
-            # for event in pygame.event.get():
-            #     if event.type == pygame.QUIT:
-            #         print('Quitting...')
-            #         pygame.quit()  # Close Window
-            #         quit()  # end pygame
+
+
+            # AULA 2 -->> 50:00
